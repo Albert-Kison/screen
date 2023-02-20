9c50dde708aefce8137f3cee71350e1668dea410
Albert Kison <albert.kison@student.griffith.ie>
2023-02-20 16:34:23 +0000

    Add screen update

--- screen.js
+++ screen.js
@@ -14,31 +14,39 @@ let pixel_map = [];
 for (let i = 0; i < HEIGHT; i++) {
     let row = [];
     for (let j = 0; j < WIDTH; j++) {
-        let r = Math.random() * 256;
-        let g = Math.random() * 256;
-        let b = Math.random() * 256;
-        row.push(`rgb(${r}, ${g}, ${b})`);
+        // let r = Math.random() * 256;
+        // let g = Math.random() * 256;
+        // let b = Math.random() * 256;
+        row.push(`rgb(0, 0, 0)`);
     }
     pixel_map.push(row);
 }
 
-let y = 0;
-for (let i = 0; i < pixel_map.length; i++) {
-    let x = 0;
-    for (let j = 0; j < pixel_map[i].length; j++) {
-        ctx.beginPath();
-        ctx.rect(x, y, PIXEL_SIZE, PIXEL_SIZE);
-        ctx.fillStyle = pixel_map[i][j];
-        ctx.fill();
-
-        x += PIXEL_SIZE;
+const drawScreen = function(pixel_map) {
+    let y = 0;
+    for (let i = 0; i < pixel_map.length; i++) {
+        let x = 0;
+        for (let j = 0; j < pixel_map[i].length; j++) {
+            ctx.beginPath();
+            ctx.rect(x, y, PIXEL_SIZE, PIXEL_SIZE);
+            ctx.fillStyle = pixel_map[i][j];
+            ctx.fill();
+    
+            x += PIXEL_SIZE;
+        }
+        y += PIXEL_SIZE;
     }
-    y += PIXEL_SIZE;
+};
+
+setTimeout(function() {
+    pixel_map[3][4] = `rgb(0, 255, 0)`;
+}, 3000);
+
+
+function animate() {
+    requestAnimationFrame(animate)
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawScreen(pixel_map);
 }
 
-// function animate() {
-//     requestAnimationFrame(animate)
-//     ctx.clearRect(0, 0, canvas.width, canvas.height);
-// }
-
-// animate();
+animate();
